aafc2646fb8fb7a3d749563eec3235c0c59e0310
_derlogoo <logoblog21@gmail.com>
2024-12-08 17:16:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["_𝖏𝖘𝖙.𝖑𝖌𝖔𝖔", "🎮 Playing 🇩🇪RP | Voice"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["_𝖏𝖘𝖙.𝖑𝖌𝖔𝖔", "🎮 Playing GRP {VC}"];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
